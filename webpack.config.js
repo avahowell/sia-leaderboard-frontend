c28bbdc7c13eebde3ccad09bef1d004de2c3f00f
Johnathan Howell <me@johnathanhowell.com>
2017-01-23 13:11:28 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+	output: {
+		path: path.resolve("./dist"),
+		filename: 'main.js'
+	},
+	resolve: {
+		root: path.resolve('./node_modules')
+	}, 
+	resolveLoader: {
+		root: path.resolve('./node_modules'),
+	},
+	node: {
+		__dirname: false,
+		__filename: false,
+	},
+	target: 'web',
+}
