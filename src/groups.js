02a85a59cb893403a24d7e5bb1de1fcf8b7c687a
Johnathan Howell <me@johnathanhowell.com>
2017-01-24 21:33:57 -0500

    pull api data in groups page

--- src/groups.js
+++ src/groups.js
@@ -2,25 +2,18 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import { List } from 'immutable'
 import GroupLeaderboard from './groupleaderboard.js'
+import request from 'superagent'
+import { parseLeaders } from './parse.js'
 
-// pull in and parse entries from the API here...
-// using testdata for now
-const testEntries = List([
-	{ name: 'testuser1', size: 1e9, lastUpdated: new Date(), groups: ['group1']},
-	{ name: 'testuser2', size: 1e9*5, lastUpdated: new Date(), groups: ['group2']},
-	{ name: 'testuser3', size: 1e9*2, lastUpdated: new Date(), groups: ['group1']},
-	{ name: 'testuser9', size: 1e9*0.5, lastUpdated:  new Date(), groups: ['group1', 'group2']},
-	{ name: 'testuser5', size: 1e9*0.2, lastUpdated: new Date(1000), groups: ['group1']},
-])
+request.get('/leaderboard').end((err, res) => {
+	if (err) {
+		console.error(err)
+	}
+	const leaders = parseLeaders(JSON.parse(res.text))
 
-const render = () => {
 	ReactDOM.render(
-		<GroupLeaderboard
-			entries={testEntries}
-		/>,
+		<GroupLeaderboard entries={leaders} />,
 		document.getElementById('react-root')
 	)
-}
-
-render()
+})
 
--- src/parse.js
+++ src/parse.js
@@ -5,6 +5,7 @@ import { List } from 'immutable'
 export const parseLeaders = (leaderArray) =>
 	List(leaderArray.map((leader) => {
 		const ret = {groups: [], name: leader.name, lastUpdated: new Date(leader.timestamp*1000), size: leader.size}
+		console.log(leader)
 		if (typeof leader.groups !== 'undefined') {
 			ret.groups = leader.groups
 		}
